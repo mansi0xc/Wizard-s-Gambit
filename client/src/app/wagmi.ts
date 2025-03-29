1d9df4651c5ca1861d46729e3d12094370bd6965
Samk <sampurnapyne1710@gmail.com>
2025-03-29 14:31:56 +0530

    wagmi configs

---
+++ client/src/app/wagmi.ts
@@ -0,0 +1,25 @@
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+  avalancheFuji
+} from 'wagmi/chains';
+
+export const config = getDefaultConfig({
+  appName: 'RainbowKit demo',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    sepolia,
+    avalancheFuji
+  ],
+  ssr: true,
+});
