c0cc9477b67d543ba4b8d99623678f2227ef85c9
mansi0xc <mansi.tibrewal76@gmail.com>
2025-03-30 09:32:45 +0000

    marketplace added

--- web3/contracts/RuneToken.sol
+++ web3/contracts/RuneToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Rune is ERC20, AccessControl {
+contract RuneToken is ERC20, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     uint256 public constant INITIAL_PLAYER_TOKENS = 1000 * 10 ** 18; // 1000 tokens
