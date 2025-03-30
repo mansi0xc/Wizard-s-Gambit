d74c43ae5cf72ad3d187a8b1c9cf7e0e79551c35
mansi0xc <mansi.tibrewal76@gmail.com>
2025-03-30 10:46:30 +0000

    readme created

--- web3/contracts/BattleArena.sol
+++ web3/contracts/BattleArena.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.22;
 
-import "./ArcaneTokenERC20.sol";
+import "./RuneToken.sol";
 
 contract BattleArena {
-    ArcaneToken public arcane;
+    RuneToken public arcane;
     uint256 public rewardAmount = 50 * 10 ** 18; // Default reward amount
 
     event BattleResult(address winner, uint256 reward);
 
-    constructor(address _arcane) {
-        arcane = ArcaneToken(_arcane);
+    constructor(address _rune) {
+        arcane = RuneToken(_rune);
     }
 
     function setRewardAmount(uint256 _rewardAmount) external {
