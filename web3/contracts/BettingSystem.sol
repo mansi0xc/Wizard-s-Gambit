a8cbe67c6aef561c3d3a6cf54880fe75ef24f922
roshankhanxox <64058135+roshankhanxox@users.noreply.github.com>
2025-04-13 19:09:56 +0530

    contracts updated

--- web3/contracts/BettingSystem.sol
+++ web3/contracts/BettingSystem.sol
@@ -2,12 +2,11 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract BettingSystem is AccessControl {
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-
+contract BettingSystem {
     IERC20 public arcaneToken;
+    uint256 public constant PLATFORM_FEE_PERCENT = 20;
+    uint256 public platformFeeBalance;
 
     struct Bet {
         address bettor;
@@ -15,93 +14,144 @@ contract BettingSystem is AccessControl {
         address chosenPlayer;
     }
 
+    struct Battle {
+        address player1;
+        address player2;
+        bool exists;
+    }
+
     struct BattleBets {
         uint256 totalBetsOnPlayer1;
         uint256 totalBetsOnPlayer2;
-        mapping(address => Bet) bets;
+        Bet[] bets;
         bool isSettled;
     }
 
+    mapping(uint256 => Battle) public battles;
     mapping(uint256 => BattleBets) public battleBets;
 
-    event BetPlaced(uint256 indexed battleId, address indexed bettor, address indexed chosenPlayer, uint256 amount);
-    event BetsSettled(uint256 indexed battleId, address indexed winner);
+    event BattleRegistered(
+        uint256 indexed battleId,
+        address indexed player1,
+        address indexed player2
+    );
+    event BetPlaced(
+        uint256 indexed battleId,
+        address indexed bettor,
+        address indexed chosenPlayer,
+        uint256 amount
+    );
+    event BetsSettled(
+        uint256 indexed battleId,
+        address indexed winner,
+        uint256 platformFee
+    );
 
     constructor(address _arcaneToken) {
         arcaneToken = IERC20(_arcaneToken);
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(ADMIN_ROLE, msg.sender);
     }
 
-    /// @notice Place a bet on a battle
-    /// @param battleId The ID of the battle
-    /// @param chosenPlayer The address of the player the bettor is betting on
-    /// @param amount The amount of ArcaneTokens to bet
-    function placeBet(uint256 battleId, address chosenPlayer, uint256 amount) external {
+    function registerBattle(
+        uint256 battleId,
+        address player1,
+        address player2
+    ) external {
+        require(!battles[battleId].exists, "Battle already exists");
+        require(
+            player1 != address(0) && player2 != address(0),
+            "Invalid player addresses"
+        );
+        require(player1 != player2, "Players must be different");
+
+        battles[battleId] = Battle(player1, player2, true);
+        emit BattleRegistered(battleId, player1, player2);
+    }
+
+    function placeBet(
+        uint256 battleId,
+        address chosenPlayer,
+        uint256 amount
+    ) external {
         require(amount > 0, "Bet amount must be greater than zero");
-        require(arcaneToken.transferFrom(msg.sender, address(this), amount), "Bet transfer failed");
+        require(
+            arcaneToken.transferFrom(msg.sender, address(this), amount),
+            "Bet transfer failed"
+        );
 
         BattleBets storage bets = battleBets[battleId];
         require(!bets.isSettled, "Bets are already settled for this battle");
 
-        // Ensure the bettor hasn't already placed a bet
-        require(bets.bets[msg.sender].amount == 0, "Bettor has already placed a bet");
+        Battle storage battle = battles[battleId];
+        require(battle.exists, "Battle does not exist");
+        require(
+            chosenPlayer == battle.player1 || chosenPlayer == battle.player2,
+            "Invalid chosen player"
+        );
 
-        // Record the bet
-        bets.bets[msg.sender] = Bet({
-            bettor: msg.sender,
-            amount: amount,
-            chosenPlayer: chosenPlayer
-        });
+        bets.bets.push(
+            Bet({
+                bettor: msg.sender,
+                amount: amount,
+                chosenPlayer: chosenPlayer
+            })
+        );
 
-        // Update total bets
-        if (chosenPlayer == address(1)) {
+        if (chosenPlayer == battle.player1) {
             bets.totalBetsOnPlayer1 += amount;
-        } else if (chosenPlayer == address(2)) {
-            bets.totalBetsOnPlayer2 += amount;
         } else {
-            revert("Invalid chosen player");
+            bets.totalBetsOnPlayer2 += amount;
         }
 
         emit BetPlaced(battleId, msg.sender, chosenPlayer, amount);
     }
 
-    /// @notice Settle bets for a battle
-    /// @param battleId The ID of the battle
-    /// @param winner The address of the winning player
-    function settleBets(uint256 battleId, address winner) external onlyRole(ADMIN_ROLE) {
+    function settleBets(uint256 battleId, address winner) external {
         BattleBets storage bets = battleBets[battleId];
         require(!bets.isSettled, "Bets are already settled for this battle");
 
+        Battle storage battle = battles[battleId];
+        require(battle.exists, "Battle does not exist");
+        require(
+            winner == battle.player1 || winner == battle.player2,
+            "Invalid winner address"
+        );
+        require(
+            msg.sender == battle.player1 || msg.sender == battle.player2,
+            "Only battle players can settle bets"
+        );
+
         uint256 totalPool = bets.totalBetsOnPlayer1 + bets.totalBetsOnPlayer2;
-        uint256 winnerPool = winner == address(1) ? bets.totalBetsOnPlayer1 : bets.totalBetsOnPlayer2;
+        uint256 platformFee = (totalPool * PLATFORM_FEE_PERCENT) / 100;
+        platformFeeBalance += platformFee;
+
+        uint256 remainingPool = totalPool - platformFee;
+        uint256 winnerPool = winner == battle.player1
+            ? bets.totalBetsOnPlayer1
+            : bets.totalBetsOnPlayer2;
 
         require(winnerPool > 0, "No bets placed on the winner");
 
-        // Distribute rewards to winning bettors
-        // Since we can't iterate over mappings directly, we need to track bettors separately
-        address[] memory bettors = new address[](0); // Create an empty array for now
-        
-        // For now, we'll just check if a specific bettor has won
-        // In a full implementation, you would need to track all bettors' addresses
-        for (uint256 i = 0; i < bettors.length; i++) {
-            address bettor = bettors[i];
-            Bet storage bet = bets.bets[bettor];
+        for (uint256 i = 0; i < bets.bets.length; i++) {
+            Bet storage bet = bets.bets[i];
             if (bet.chosenPlayer == winner) {
-                uint256 reward = (bet.amount * totalPool) / winnerPool;
-                require(arcaneToken.transfer(bet.bettor, reward), "Reward transfer failed");
+                uint256 reward = (bet.amount * remainingPool) / winnerPool;
+                require(
+                    arcaneToken.transfer(bet.bettor, reward),
+                    "Reward transfer failed"
+                );
             }
         }
 
         bets.isSettled = true;
-
-        emit BetsSettled(battleId, winner);
+        emit BetsSettled(battleId, winner, platformFee);
     }
 
-    /// @notice Withdraw ArcaneTokens from the contract (for admin use)
-    /// @param to Address to send the tokens to
-    /// @param amount Amount of tokens to withdraw
-    function withdrawArcaneTokens(address to, uint256 amount) external onlyRole(ADMIN_ROLE) {
+    function withdrawPlatformFees(address to, uint256 amount) external {
+        require(
+            amount <= platformFeeBalance,
+            "Insufficient platform fee balance"
+        );
+        platformFeeBalance -= amount;
         require(arcaneToken.transfer(to, amount), "Withdrawal failed");
     }
 }
--- web3/contracts/PlayerAvatar.sol
+++ web3/contracts/PlayerAvatar.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Rune.sol";
 
-contract PlayerAvatarNFT is ERC721URIStorage,ERC721Burnable, Ownable {
+contract PlayerAvatarNFT is ERC721URIStorage, ERC721Burnable, Ownable {
     Rune public rune;
     mapping(address => uint256) private _ownedToken;
     uint256 private _nextTokenId = 1;
@@ -64,21 +64,15 @@ contract PlayerAvatarNFT is ERC721URIStorage,ERC721Burnable, Ownable {
         return (tokenId, tokenURI(tokenId));
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (bool)
-    {
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
